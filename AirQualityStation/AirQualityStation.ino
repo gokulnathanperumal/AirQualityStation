f25d97ab801e9dbdb2fc40a89a2ba34653f1ae01
Gokulnathan Perumal <gokulnathanpeee@gmail.com>
2023-09-03 23:14:52 +0530

    Arduino Empty Project

---
+++ AirQualityStation/AirQualityStation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AirQualityStation.ino
+ Created:	9/3/2023 11:09:26 PM
+ Author:	Gokul
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
